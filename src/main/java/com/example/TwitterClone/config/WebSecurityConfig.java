8d7890edb6ddcf1be227098d78141b0b67b9cefe
EugeniySichev <sichev91@inbox.ru>
2021-04-28 14:55:44 +0300

    added the ability to identify the author of the post

--- src/main/java/com/example/TwitterClone/config/WebSecurityConfig.java
+++ src/main/java/com/example/TwitterClone/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.TwitterClone.config;
 
+import com.example.TwitterClone.service.UserService;
 import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -21,7 +22,10 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private DataSource dataSource;
+    private UserService userService;
+
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -39,11 +43,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("select username, password, active from usr where username=?")
-                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.io = ur.user_id where u.username=?");
+        auth.userDetailsService(userService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
 
