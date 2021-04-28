43f3e68be3ed1cebd56fddd17f39612225af99d1
EugeniySichev <sichev91@inbox.ru>
2021-04-28 03:19:37 +0300

    add registration system

--- src/main/java/com/example/TwitterClone/controller/MainController.java
+++ src/main/java/com/example/TwitterClone/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.TwitterClone;
+package com.example.TwitterClone.controller;
 
 
 import com.example.TwitterClone.domain.Message;
@@ -12,20 +12,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
 
@@ -34,7 +30,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
 
---
+++ src/main/java/com/example/TwitterClone/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.TwitterClone.domain;
+
+public enum Role {
+    USER;
+}
